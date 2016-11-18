9b7551b122c78f819c68cee81141db23a7f1bafc
katie80 <k.smyth.9288@gmail.com>
2016-11-17 17:00:51 -0700

    DemoDay final commit

--- public/js/submit.js
+++ public/js/submit.js
@@ -1,14 +1,24 @@
+/*jshint esversion:6*/
+function getIngredients() {
+	var ingredients = [];
+	$('.duplicateInput').each(function(index) {
+
+	var ingredient = {number: $(this).find('.ingredientNumber').val(), unit: $(this).find('.ingredientUnit').val(), name: $(this).find('.ingredientName').val()};
+    ingredients.push(ingredient);
+});
+	console.log(ingredients);
+
+	return ingredients;
+}
+
 function submitRecipe() {
+	var ingredients = getIngredients();
 	var recipe = {
 		name: $("#recipeName").val(),
 		author: $("#recipeAuthor").val(),
 		year: $("#recipeYear").val(),
 		instructions: $("#recipeInstructions").val(),
-		ingredients: JSON.stringify([{
-			number: $(".ingredientNumber").val(), 
-			unit: $(".ingredientUnit").val(), 
-			name: $(".ingredientName").val()
-		}])
+		ingredients: JSON.stringify(ingredients)
 	};
 	console.log(recipe.ingredients);
 
@@ -16,8 +26,13 @@ function submitRecipe() {
 		console.log(res);
 		// window.location= "/recipe/" + res._id;
 	});
-}
 
+	// $('.duplicateInput').each(function(index, element) => {
+	// 	ingredients.push(element.find('.ingredientNumber').val());
+	// 	ingredients.push(element.find('.ingredientUnit').val());
+	// 	ingredients.push(element.find('.ingredientName').val());
+	// });
+}
 
 
 $("#send_new_recipe").click(submitRecipe);
